0d477ed374b335acfdd8c1ca4302a1e9d5e26363
ozgur-onal <thefestinalente@gmail.com>
2025-03-13 14:12:53 +0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: ProcessingInstruction.env.VITE_BASE_PATH || "/react-vite-dash"
+  base: Process.env.VITE_BASE_PATH || "/react-vite-dash"
 })
